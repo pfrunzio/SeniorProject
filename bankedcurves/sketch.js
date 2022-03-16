12b6bcbd729ad60c241dbdcc18b4ca9b827a31bb
masterbbud <masterbbud@gmail.com>
2022-03-16 18:44:24 -0400

    BASIC TRACK + CAR

--- bankedcurves/sketch.js
+++ bankedcurves/sketch.js
@@ -3,20 +3,51 @@ var angle;
 var fcoeff;
 var speed;
 var gravity;
+var trackResolution;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
+  trackResolution = 1;
 }
 
 function draw() {
   background(220);
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
 
-  translate(-240, -100, 0);
-  normalMaterial();
+  ambientLight(180);
+  //pointLight(255, 255, 255, locX, locY, 100);
+  drawTrack();
+
+}
+function drawTrack() {
   push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
+  rotateZ(0);
+  rotateX(-1);
+  rotateY(0);
+  normalMaterial();
+  noStroke();
+
+  cone(70,20,16,16,false);
+
+  push();
+  translate(0,0,-10);
+  fill(0);
+  rotateX(-PI/2);
+  plane(800);
+  pop();
+
+  push();
   rotateY(frameCount * 0.01);
-  plane(70);
+
+
+  push();
+  rotateZ(1);
+  translate(0,-40,0);
+  box(20);
+  pop();
+
+  pop();
+
   pop();
 }
