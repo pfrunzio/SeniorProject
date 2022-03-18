11c580dd0f649d612d0d0377e7912048b74708b1
Brandon Faunce <masterbbud@gmail.com>
2022-03-18 11:12:07 -0400

    Working 3D Track (with angles & velocity)

--- bankedcurves/sketch.js
+++ bankedcurves/sketch.js
@@ -4,10 +4,24 @@ var fcoeff;
 var speed;
 var gravity;
 var trackResolution;
+var trackAbove;
+var carHeight;
+var carLength;
+var carWidth;
+var carPos;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
   trackResolution = 1;
+  angle = 60;
+  speed = 100;
+  radius = 140;
+  trackAbove = 40;
+  carHeight = 10;
+  carLength = 25;
+  carWidth = 15;
+  carPos = 0;
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -15,36 +29,52 @@ function draw() {
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
 
-  ambientLight(180);
-  //pointLight(255, 255, 255, locX, locY, 100);
+  ambientLight(100);
+  pointLight(255, 255, 255, 0, 0, -100);
   drawTrack();
-
+  let fr = 1;
+  if (frameRate() != 0){
+    fr = frameRate();
+  }
+  carPos += 180 * speed / (PI*radius*fr);
 }
 function drawTrack() {
   push();
-  rotateZ(0);
-  rotateX(-1);
+  rotateZ(0*frameCount*0.5);
+  rotateX(-70);
   rotateY(0);
-  normalMaterial();
+  fill(255);
   noStroke();
 
-  cone(70,20,16,16,false);
+  push();
+  let habove = trackAbove*sin(angle);
+  let bigradius = (trackAbove/2)*sin(90-angle) + radius;
+  let h = bigradius * (sin(angle)/sin(90-angle));
+  translate(0,h/2-habove,0);
+  cone(bigradius,h,64,16,false);
+  pop();
 
   push();
-  translate(0,0,-10);
+  translate(0,0,0);
   fill(0);
-  rotateX(-PI/2);
+  rotateX(-90);
   plane(800);
   pop();
 
   push();
-  rotateY(frameCount * 0.01);
+  translate(0,-habove/2,0);
+  rotateY(carPos%360);
 
 
   push();
-  rotateZ(1);
-  translate(0,-40,0);
-  box(20);
+  //rotateZ(angle);
+  translate(radius,0,0);
+  rotateZ(-angle);
+  translate(0,-carHeight/2-1,0);
+  //translate(radius*sin(angle),-radius*sin(90-angle),0);
+  //translate(radius*sin(90-angle),radius*sin(angle),0);
+  fill(255,0,0);
+  box(carWidth,carHeight,carLength);
   pop();
 
   pop();
