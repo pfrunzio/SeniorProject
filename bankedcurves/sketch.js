dbfe363cf2602849f2450c4fd15dfb0171a1c870
pfrunzio <pfrunzio22@students.hopkins.edu>
2022-03-30 23:47:59 -0400

    fully transfered to instance mode (https://p5js.org/reference/#/p5/p5)

--- bankedcurves/sketch.js
+++ bankedcurves/sketch.js
@@ -12,9 +12,9 @@ var carPos;
 var wheelRadius;
 var ui;
 
-new p5(function() {
-  setup = function() {
-    createCanvas(800, 600, WEBGL);  // <--- WEBGL
+var t = function(p) {
+  p.setup = function() {
+    p.createCanvas(800, 600, p.WEBGL);
     trackResolution = 1;
     angle = 20;
     speed = 500;
@@ -25,125 +25,130 @@ new p5(function() {
     carWidth = 15;
     carPos = 0;
     wheelRadius = 5;
-    angleMode(DEGREES);
+    p.angleMode(p.DEGREES);
   };
-  draw = function() {
-    background(220);
-    let locX = mouseX - height / 2;
-    let locY = mouseY - width / 2;
+  p.draw = function() {
+    p.background(220);
+    let locX = p.mouseX - p.height / 2;
+    let locY = p.mouseY - p.width / 2;
 
-
-    ambientLight(140);
+    p.ambientLight(140);
     //pointLight(2550, 2550, 2550, 0, 0, -1*frameCount);
 
-    drawTrack();
+    //red dots
+    p.strokeWeight(10);
+    p.stroke(255,0,0);
+    p.quad(0,0,0,0,0,0,0,0,0,0,0,0);
+    p.point(10,-90,0);
+    p.point(-10,-90,0);
+
+    drawTrack(p);
 
     let fr = 1;
-    if (frameRate() != 0) {
-      fr = frameRate();
+    if (p.frameRate() != 0) {
+      fr = p.frameRate();
     }
-    carPos += 180 * speed / (PI*radius*fr);
+    carPos += 180 * speed / (p.PI*radius*fr);
   };
-});
+};
+var myp5 = new p5(t, "track");
 
-new p5(function(ui) {
-  ui.setup = function() {
-    ui.createCanvas(100, 600, ui.P2D);
+var u = function(p) {
+  p.setup = function() {
+    p.createCanvas(100, 600, p.P2D);
   };
-  ui.draw = function() {
-    ui.background(0);
+  p.draw = function() {
+    p.background(50,50,50);
 
-    drawUI();
+    drawUI(p);
   };
-});
+};
+var myp5 = new p5(u, 'ui');
 
-function drawTrack() {
-  strokeWeight(10);
-  stroke(255,0,0);
-  quad(0,0,0,0,0,0,0,0,0,0,0,0);
-  point(10,-90,0);
-  point(-10,-90,0);
-  push(); // camera
-    translate(-200,-100,0);
-    rotateZ(0*frameCount*0.5);
-    rotateX(-70);
-    rotateY(0);
+function drawTrack(p = p5.instance) {
 
-    directionalLight(160,160,160,0,-1,0);
-    fill(100);
-    circle(0,0,0,0,0);
+  p.push(); // camera
+    p.translate(-200,-100,0);
+    p.rotateZ(0*p.frameCount*0.5);
+    p.rotateX(-70);
+    p.rotateY(0);
 
-    noStroke();
+    p.directionalLight(160,160,160,0,-1,0);
+
+    p.fill(100);
+    p.circle(0,0,0,0,0);
+
+    p.noStroke();
 
 
-    push(); // track
-      let habove = trackAbove*sin(angle);
-      let bigradius = (trackAbove/2)*sin(90-angle) + radius;
-      let h = bigradius * (sin(angle)/sin(90-angle));
-      translate(0,h/2-habove,0);
-      cone(bigradius,h,64,16,false);
-      fill(0);
+    p.push(); // track
+      let habove = trackAbove*p.sin(angle);
+      let bigradius = (trackAbove/2)*p.sin(90-angle) + radius;
+      let h = bigradius * (p.sin(angle)/p.sin(90-angle));
+      p.translate(0,h/2-habove,0);
+      p.cone(bigradius,h,64,16,false);
+      p.fill(0);
 
-      fill(20);
-      cylinder(bigradius,h,64,16,false,false);
-    pop();
+      p.fill(20);
+      p.cylinder(bigradius,h,64,16,false,false);
+    p.pop();
 
-    push(); // ground
-      translate(0,10,0);
-      fill(255);
-      rotateX(0);
-      cylinder(149,20);
-    pop();
+    p.push(); // ground
+      p.translate(0,10,0);
+      p.fill(255);
+      p.rotateX(0);
+      p.cylinder(149,20);
+    p.pop();
 
-    push(); // car
+    p.push(); // car
 
-      translate(0,-habove/2,0);
-      rotateY(carPos%360);
+      p.translate(0,-habove/2,0);
+      p.rotateY(carPos%360);
 
 
-      push(); // car body
+      p.push(); // car body
         //rotateZ(angle);
-        stroke(0);
-        translate(radius,0,0);
-        rotateZ(-angle);
-        translate(0,-carHeight/2-1-wheelRadius,0);
+        p.stroke(0);
+        p.translate(radius,0,0);
+        p.rotateZ(-angle);
+        p.translate(0,-carHeight/2-1-wheelRadius,0);
         //translate(radius*sin(angle),-radius*sin(90-angle),0);
         //translate(radius*sin(90-angle),radius*sin(angle),0);
-        fill(255,0,0);
-        box(carWidth,carHeight,carLength);
-        fill(200);
-        strokeWeight(1);
-        var rot = -(((radius*(carPos*PI/180))%(2*PI*wheelRadius))/(2*PI*wheelRadius))*360;
-        push(); // wheel 1
-          translate(carWidth/2,carHeight/2,carLength/2-wheelRadius-2);
-          rotateZ(90);
-          rotateY(rot);
-          cylinder(wheelRadius,2,8,4,true,true);
-        pop();
-        push(); // wheel 2
-          translate(carWidth/2,carHeight/2,-carLength/2+wheelRadius+2);
-          rotateZ(90);
-          rotateY(rot);
-          cylinder(wheelRadius,2,8,4,true,true);
-        pop();
-        push(); // wheel 3
-          translate(-carWidth/2,carHeight/2,carLength/2-wheelRadius-2);
-          rotateZ(90);
-          rotateY(rot);
-          cylinder(wheelRadius,2,8,4,true,true);
-        pop();
-        push(); // wheel 4
-          translate(-carWidth/2,carHeight/2,-carLength/2+wheelRadius+2);
-          rotateZ(90);
-          rotateY(rot);
-          cylinder(wheelRadius,2,8,4,true,true);
-        pop();
-      pop();
-    pop();
-  pop();
+        p.fill(255,0,0);
+        p.box(carWidth,carHeight,carLength);
+        p.fill(200);
+        p.strokeWeight(1);
+        var rot = -(((radius*(carPos*p.PI/180))%(2*p.PI*wheelRadius))/(2*p.PI*wheelRadius))*360;
+        p.push(); // wheel 1
+          p.translate(carWidth/2,carHeight/2,carLength/2-wheelRadius-2);
+          p.rotateZ(90);
+          p.rotateY(rot);
+          p.cylinder(wheelRadius,2,8,4,true,true);
+        p.pop();
+        p.push(); // wheel 2
+          p.translate(carWidth/2,carHeight/2,-carLength/2+wheelRadius+2);
+          p.rotateZ(90);
+          p.rotateY(rot);
+          p.cylinder(wheelRadius,2,8,4,true,true);
+        p.pop();
+        p.push(); // wheel 3
+          p.translate(-carWidth/2,carHeight/2,carLength/2-wheelRadius-2);
+          p.rotateZ(90);
+          p.rotateY(rot);
+          p.cylinder(wheelRadius,2,8,4,true,true);
+        p.pop();
+        p.push(); // wheel 4
+          p.translate(-carWidth/2,carHeight/2,-carLength/2+wheelRadius+2);
+          p.rotateZ(90);
+          p.rotateY(rot);
+          p.cylinder(wheelRadius,2,8,4,true,true);
+        p.pop();
+      p.pop();
+    p.pop();
+  p.pop();
 };
 
-function drawUI() {
-  fill(255);
-  rect(20,20,50,50);
+function drawUI(p = p5.instance) {
+  p.fill(255);
+  p.rect(20,20,50,50);
 };
