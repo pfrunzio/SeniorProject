3db661dcc56bccd396838a8ec5efa311af6e89a7
pfrunzio <pfrunzio22@students.hopkins.edu>
2022-04-05 15:35:59 -0400

    fixed fbd arrows, added cart switch, added broken slider

--- bankedcurves/sketch.js
+++ bankedcurves/sketch.js
@@ -12,9 +12,12 @@ var carWidth;
 var carPos;
 var wheelRadius;
 var ui;
+var radiusSlider = document.getElementById("radiusSlider");
 
 var t = function(p) {
   p.setup = function() {
+    // setup HTML elements
+
     p.createCanvas(800, 600, p.WEBGL);
     trackResolution = 1;
     angle = 40;
@@ -37,14 +40,7 @@ var t = function(p) {
     let locY = p.mouseY - p.width / 2;
 
     p.ambientLight(140);
-    //pointLight(2550, 2550, 2550, 0, 0, -1*frameCount);
-
-    //red dots
-    p.strokeWeight(10);
-    p.stroke(255,0,0);
-    p.quad(0,0,0,0,0,0,0,0,0,0,0,0);
-    p.point(10,-90,0);
-    p.point(-10,-90,0);
+    //pointLight(2550, 2550, 2550, 0, 0, -1*frameCount)
 
     drawTrack(p);
 
@@ -53,21 +49,26 @@ var t = function(p) {
       fr = p.frameRate();
     }
     carPos += 180 * speed / (p.PI*radius*fr);
+
   };
 };
+
+// radiusSlider.oninput = function() {
+//   radius = this.value;
+// }
 var myp5 = new p5(t, "track");
 
-var u = function(p) {
-  p.setup = function() {
-    p.createCanvas(100, 600, p.P2D);
-  };
-  p.draw = function() {
-    p.background(50,50,50);
-
-    drawUI(p);
-  };
-};
-var myp5 = new p5(u, 'ui');
+// var u = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(100, 600, p.P2D);
+//   };
+//   p.draw = function() {
+//     p.background(50,50,50);
+//
+//     drawUI(p);
+//   };
+// };
+// var myp5 = new p5(u, 'ui');
 
 var f = function(p) {
   p.setup = function() {
@@ -183,6 +184,8 @@ function drawFBD(p = p5.instance){
   }
   let hyp = p.sqrt(h*h+w*w);
   p.fill(0);
+  p.stroke(0);
+  p.strokeWeight(3);
   p.triangle(100-w/2,100-h/2,100-w/2,100+h/2,100+w/2,100+h/2);
 
   // cart
@@ -190,37 +193,60 @@ function drawFBD(p = p5.instance){
     p.translate(100-w/2,100-h/2);
     p.rotate(angle);
     p.translate(hyp/2,0);
-    p.fill(255,0,0);
-    p.rect(-carLength/2,-carHeight-wheelRadius,carLength,carHeight);
-    p.ellipseMode(p.CENTER);
-    p.fill(200);
-    p.circle(-carLength/2+wheelRadius+2,-wheelRadius,wheelRadius*2);
-    p.circle(carLength/2-wheelRadius-2,-wheelRadius,wheelRadius*2);
+
+    if (cartSwitch == true) {
+      p.fill(255,0,0);
+      p.rect(-carLength/2,-carHeight-wheelRadius,carLength,carHeight);
+      p.ellipseMode(p.CENTER);
+      p.fill(200);
+      p.circle(-carLength/2+wheelRadius+2,-wheelRadius,wheelRadius*2);
+      p.circle(carLength/2-wheelRadius-2,-wheelRadius,wheelRadius*2);
+    }
 
     // vector lengths
     let F_G = mass * gravity;
     let F_N = F_G * p.cos(angle);
     // mv^2/r = F_Gsin - F_fs
-    let F_fs = F_G * p.sin(angle) - mass * speed * speed / radius;
+    let F_fs = (F_G -F_N*p.cos(angle))/p.sin(angle);
 
+    let values = [F_G,F_N,F_fs];
+    values = clamp(p,values,50);
     // arrows
     // positive x is down the slope, positive y is into the slope
-    drawArrow(p,p.createVector(0,0),p.createVector(0,-F_N),'green',3,7);
-    drawArrow(p,p.createVector(0,0),p.createVector(F_fs,0),'green',3,7);
+    drawArrow(p,p.createVector(0,0),p.createVector(0,-values[1]),'green',3,7);
+    drawArrow(p,p.createVector(0,0),p.createVector(-values[2],0),'green',3,7);
     p.rotate(-angle);
-    drawArrow(p,p.createVector(0,0),p.createVector(0,F_G),'green',3,7);
+    drawArrow(p,p.createVector(0,0),p.createVector(0,values[0]),'green',3,7);
   p.pop();
+
 }
+
+
 
 function drawArrow(p = p5.instance, start, end, color, width, size){
   p.push();
-  p.stroke(color);
-  p.strokeWeight(3);
-  p.fill(color);
-  p.translate(start.x,start.y);
-  p.line(0,0,end.x,end.y);
-  p.rotate(end.heading());
-  p.translate(end.mag() - size, 0);
-  p.triangle(0, size/2, 0, -size/2, size, 0);
+    p.stroke(color);
+    p.strokeWeight(3);
+    p.fill(color);
+    p.translate(start.x,start.y);
+    p.line(0,0,end.x,end.y);
+    p.rotate(end.heading());
+    p.translate(end.mag() - size, 0);
+    p.triangle(0, size/2, 0, -size/2, size, 0);
   p.pop();
+}
+
+function clamp(p = p5.instance, floatList, toClamp){
+  let max = 0;
+  for (let i = 0; i < floatList.length; i++){
+    if (p.abs(floatList[i]) > max){
+      max = p.abs(floatList[i]);
+    }
+  }
+  let ret = [];
+  let ratio = toClamp/max;
+  for (let i = 0; i < floatList.length; i++){
+    ret[i] = floatList[i] * ratio;
+  }
+  return ret;
 }
